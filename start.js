2e85f63aaaa97845b4fd162214e974307e361f7f
Prashant Pawar <prashantpawar@gmail.com>
2020-01-18 21:10:39 -0500

    - Now doing bulk testing of parameters

--- start.js
+++ start.js
@@ -1,10 +1,13 @@
+const R = require('ramda');
 const program = require('commander');
 const { ETL, run } = require('./index');
 
+/**
 const desiredAllocation = {
     BTC: 0.8,
     USD: 0.2
 };
+**/
 const initialPortfolio = {
     BTC: 10,
     USD: 80000
@@ -19,6 +22,16 @@ program.parse(process.argv);
 
 if (program.debug) console.log(program.opts());
 Promise.resolve(program.opts())
-.then(ETL)
-.then(run(desiredAllocation, initialPortfolio))
-.catch(console.error);
+    .then(ETL)
+    .then((options) => {
+        const tens = R.reject(R.modulo(R.__, 10));
+        return R.reduce((accu, i) => {
+            return accu.then(() => run({
+                BTC: i / 100,
+                USD: (100 - i) / 100
+            }, initialPortfolio, options));
+        },
+            Promise.resolve(options),
+            tens(R.range(0, 101)));
+    })
+    .catch(console.error);
